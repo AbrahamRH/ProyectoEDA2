527a70477b044d6d339cdc61e015c13c857f2c63
AbrahamRH <abraham.esc.tra@gmail.com>
2019-11-24 22:34:01 -0600

    Modificaciones en el grafo, eliminacion de atributos y métodos no necesarios

--- Estructuras/grafo.cpp
+++ Estructuras/grafo.cpp
@@ -1,33 +1,12 @@
-/*Copyright (C)
- * 2019 - eda1 dot fiunam at yahoo dot com dot mx
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- *
- */
 
 
 #include <iostream>
-// para cin, cout, cerr. (leer del teclado e imprimir en la pantalla)
 
 #include <string>
-// para declarar y procesar cadenas
 
 #include <list>
-// para los arreglos []
 
 #include <map>
-// para los diccionarios (tablas hash)
 
 #include <array>
 
@@ -50,17 +29,15 @@ private:
 	// nodo. P. ej., Estudiante: promedio, semestre. Persona: dirección, edad;
 	// etc.
 
-	std::list<Vertex> neighbors; ///< lista de vértices adyacentes
+	std::string Simbolo;
 
-	int distance{ 0 };
+	int numeroAtomico;
+
+	std::list<Vertex> neighbors; ///< lista de vértices adyacentes
 
 	Colors color{ Colors::BLACK };
 
 	std::string  predecesor;
-
-	size_t discovery_time{ 0 };
-
-	size_t finish_time{ 0 };
 
 public:
 
@@ -75,15 +52,13 @@ public:
 	//Métodos agregados
 	//Setters
 
-	void set_distance( int distance );
-
 	void set_color( Vertex::Colors color );
 
 	void set_predecesor( std::string predecesor );
 
-	void set_discovery_time( size_t time );
+	void set_numero_atomico( int numero );
 
-	void set_finish_time( size_t time );
+	void set_nombre( std::string nombre );
 
 	//Getters
 
@@ -95,12 +70,12 @@ public:
 
 	std::list<Vertex> get_neighbors();
 
-	size_t  get_discovery_time();
 
-	size_t  get_finish_time();
-
-	//imprimir
-
+	/* -------------------------------*/
+	/**
+	 * @Synopsis Método para imprimir el contendo de un vértice
+	 */
+	/* -------------------------------*/
 	void print();
 };
 
@@ -128,11 +103,6 @@ void Vertex::print_neighbors()
 
 //Setters
 
-void Vertex::set_distance( int distance )
-{
-	this->distance = distance;
-}
-
 void Vertex::set_color( Vertex::Colors color)
 {
 	this->color = color;
@@ -143,21 +113,8 @@ void Vertex::set_predecesor( std::string predecesor )
 	this->predecesor = predecesor;
 }
 
-void Vertex::set_discovery_time( size_t time ){
-	this->discovery_time = time;
-}
-
-void Vertex::set_finish_time( size_t time ){
-	this->finish_time = time;
-}
-
 
 //Getters
-
-int Vertex::get_distance()
-{
-	return this->distance;
-}
 
 Vertex::Colors Vertex::get_color()
 {
@@ -174,23 +131,10 @@ std::list<Vertex> Vertex::get_neighbors(){
 }
 
 
-size_t  Vertex::get_discovery_time(){
-	return this->discovery_time;
-}
-
-size_t  Vertex::get_finish_time(){
-	return this->finish_time;
-}
-
-//imprimir
-
 void Vertex::print()
 {
 	std::cout << "\nName: "     << this->get_name() << std::endl;
 	std::cout << "Predecesor: " << this->get_predecesor() << std::endl;
-	std::cout << "Discovery: "  << this->get_discovery_time() << std::endl;
-	std::cout << "Finish: " 	<< this->get_finish_time() << std::endl;
-
 	std::string color_as_string;
 
 	switch( this->color ){
@@ -200,7 +144,6 @@ void Vertex::print()
 	}
 
 	std::cout << "Color: " << color_as_string << std::endl;
-
 }
 
 
@@ -216,8 +159,6 @@ private:
 	// asociamos un vértice (value) a una cadena (key):
 	// vertices[ string ] = a_vertex
 
-	static size_t time;
-
 public:
 	Graph();
 //	Graph( std::vector<Vertex> vertices );
@@ -230,31 +171,9 @@ public:
 
 	void BFS(Vertex* start);
 
-	void DFS(Vertex* start);
-
-	void dfs_traverse( Vertex* v , size_t* time );
-
-	std::vector<Vertex*> DFS_top( Vertex* start );
-
-	void dfs_traverse_top( Vertex* v , std::vector<Vertex*>* list , size_t* time );
-
-	bool add_edge_directed( std::string edge1, std::string edge2 );
-
 	Vertex* get_vertex( std::string name);
 
-	//Setter
-
-	void clear_time(){ Graph::time = 0; }
-
-	void inc_time(){ Graph::time++; }
-
-	//Getter
-
-	size_t get_time() { return Graph::time; }
-
 };
-
-size_t Graph::time = 0;
 
 Graph::Graph()
 {
@@ -288,24 +207,6 @@ bool Graph::add_edge( std::string edge1, std::string edge2 )
 	return res;
 }
 
-bool Graph::add_edge_directed( std::string edge1, std::string edge2 ){
-	bool res = false;
-	// uno, o ambos nodos, no existen
-
-	auto v1 = this->vertices.find( edge1 );
-	auto v2 = this->vertices.find( edge2 );
-
-	if( v1 != this->vertices.end() and v2 != this->vertices.end() ){
-
-		v1->second.add_neighbor( v2->second );
-
-		return true;
-	}
-
-	return res;
-
-}
-
 
 //Devuelve un apuntador a un vértice del grafo
 Vertex* Graph::get_vertex( std::string name){
@@ -325,19 +226,6 @@ void Graph::print()
 
 void Graph::BFS(Vertex* start)
 {
-
-	for( auto v : this->vertices ){
-		v.second.set_color(Vertex::Colors::BLACK);
-		v.second.set_distance(0);
-		v.second.set_predecesor("");
-	}
-
-	//Impresión del contenido de los nodos del grafo
-	std::cout << "\nDespués de la asignación inicial a los vértices" << std::endl;
-	for( auto v : this->vertices ){
-		v.second.print();
-	}
-
 	start->set_color(Vertex::Colors::WHITE);
 	std::list<Vertex*> queue;
 	queue.push_back(start);
@@ -356,7 +244,6 @@ void Graph::BFS(Vertex* start)
 
 			if( neighbor->get_color() == Vertex::Colors::BLACK ){
 				neighbor->set_color(Vertex::Colors::GRAY);
-				neighbor->set_distance(vertex->get_distance() + 1);
 				neighbor->set_predecesor(vertex->get_name());
 				neighbor->set_color(Vertex::Colors::WHITE);
 				queue.push_back(neighbor);
@@ -366,123 +253,15 @@ void Graph::BFS(Vertex* start)
 	}
 
 	//Impresión del contenido de los nodos del grafo
-	std::cout << "\nDespués asignar los valores tras recorrer el grafo" << std::endl;
 	for( auto v : this->vertices ){
 		v.second.print();
 	}
 }
 
-//std::vector<Vertex*> Graph::DFS(Vertex start)
-
-void Graph::DFS(Vertex* start)
-{
-
-	for( auto v : this->vertices ){
-		v.second.set_color(Vertex::Colors::BLACK);
-		v.second.set_predecesor("");
-		v.second.set_discovery_time(0);
-		v.second.set_finish_time(0);
-	}
-
-	size_t time = 0;
-
-	this->dfs_traverse( start , &time );
-
-	std::cout << "\nDespués asignar los valores tras recorrer el grafo" << std::endl;
-	for( auto v : this->vertices ){
-		v.second.print();
-	}
-
-}
-
-
-void Graph::dfs_traverse( Vertex* v , size_t* time)
-{
-
-	++*time;
-
-	v->set_discovery_time( *time );
-
-	v->set_color( Vertex::Colors::GRAY );
-
-	for( auto neighbors : v->get_neighbors() ){
-		Vertex* neighbor = this->get_vertex( neighbors.get_name() );
-		if( neighbor->get_color() == Vertex::Colors::BLACK ){
-			neighbor->set_predecesor( v->get_name() );
-			this->dfs_traverse( neighbor , time );
-		}
-	}
-
-	v->set_color( Vertex::Colors::WHITE );
-
-	++*time;
-
-	v->set_finish_time( *time );
-
-}
-
-/*
- * Topológico
- */
-
-std::vector<Vertex*> Graph::DFS_top( Vertex* start )
-{
-
-	for( auto v : this->vertices ){
-		v.second.set_color(Vertex::Colors::BLACK);
-		v.second.set_predecesor("");
-		v.second.set_discovery_time(0);
-		v.second.set_finish_time(0);
-	}
-
-	size_t time = 0;
-
-	std::vector<Vertex*> list;
-
-	this->dfs_traverse_top( start , &list , &time);
-
-	std::sort(  list.begin() ,
-				list.end() ,
-				[]( Vertex* lhs, Vertex* rhs )
-				{
-					return lhs->get_finish_time() > rhs->get_finish_time();
-				}
-			);
-
-	return list;
-
-}
-
-void Graph::dfs_traverse_top( Vertex* v , std::vector<Vertex*>* list , size_t* time )
-{
-	++*time;
-
-	v->set_discovery_time( *time );
-
-	v->set_color( Vertex::Colors::GRAY );
-
-	for( auto neighbors : v->get_neighbors() ){
-		Vertex* neighbor = this->get_vertex( neighbors.get_name() );
-		if( neighbor->get_color() == Vertex::Colors::BLACK ){
-			neighbor->set_predecesor(v->get_name());
-			this->dfs_traverse_top( neighbor , list , time );
-		}
-	}
-
-	v->set_color( Vertex::Colors::WHITE );
-
-	++*time;
-
-	v->set_finish_time( *time );
-
-	list->push_back( v );
-
-}
 
 int main()
 {
 	Graph g;
-/*
 	g.add_vertex( Vertex( "A" ) );
 	g.add_vertex( Vertex( "B" ) );
 	g.add_vertex( Vertex( "C" ) );
@@ -495,31 +274,20 @@ int main()
 	g.add_vertex( Vertex( "J" ) );
 
 
-	g.add_edge_directed( "A", "B" );
-	g.add_edge_directed( "B", "F" );
-	g.add_edge_directed( "F", "G" );
-	g.add_edge_directed( "F", "I" );
-	g.add_edge_directed( "G", "C" );
-	g.add_edge_directed( "G", "J" );
-	g.add_edge_directed( "J", "F" );
-	g.add_edge_directed( "I", "H" );
-	g.add_edge_directed( "H", "D" );
-	g.add_edge_directed( "D", "E" );
-	g.add_edge_directed( "E", "H" );
-	g.add_edge_directed( "E", "A" );
+	g.add_edge( "A", "B" );
+	g.add_edge( "B", "F" );
+	g.add_edge( "F", "G" );
+	g.add_edge( "F", "I" );
+	g.add_edge( "G", "C" );
+	g.add_edge( "G", "J" );
+	g.add_edge( "J", "F" );
+	g.add_edge( "I", "H" );
+	g.add_edge( "H", "D" );
+	g.add_edge( "D", "E" );
+	g.add_edge( "E", "H" );
+	g.add_edge( "E", "A" );
 
 	g.print();
-*/
-	//g.DFS(g.get_vertex("A"));
-
-	#if 1
-	std::vector<Vertex*> vertices;
-
-	vertices = g.DFS_top(g.get_vertex("A"));
-
-	for ( auto v : vertices ){
-		v->print();
-	}
-	#endif
+	g.BFS(g.get_vertex("A"));
 
 }
